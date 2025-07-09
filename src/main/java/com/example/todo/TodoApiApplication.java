aca5fcf98545ecb5f22a9b7918492a18954672c6
helenkoll <facekity@gmail.com>
2025-07-09 13:38:40 +0300

    Adding logging levels (info, warning,error , debug)

--- src/main/java/com/example/todo/TodoApiApplication.java
+++ src/main/java/com/example/todo/TodoApiApplication.java
@@ -3,7 +3,10 @@ package com.example.todo;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +14,9 @@ import com.example.todo.model.Todo;
 import com.example.todo.repository.TodoRepository;
 
 @SpringBootApplication
-public class TodoApiApplication {
+public class TodoApiApplication implements CommandLineRunner {
+
+    Logger log = LoggerFactory.getLogger(TodoApiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApiApplication.class, args);
@@ -43,5 +48,14 @@ public class TodoApiApplication {
     public void deleteTodo(Long id) {
         todoRepository.deleteById(id);
     }
-
+    
+    //adding logging 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("This is a console message!");
+        log.info("INFO:  This is an info message!");
+        log.warn("WARMING: This is a warning message!");
+        log.error("ERROR: This is an error message");
+        log.debug("DEBUG: This is a debug message");
+    }
 }
