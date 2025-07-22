2afdea19d7dc0571f30b7f6d2623a23969b019a4
helenkoll <facekity@gmail.com>
2025-07-22 12:28:17 +0300

    TodoService.java and UserService.java files

---
+++ src/main/java/com/example/todo/services/UserService.java
@@ -0,0 +1,32 @@
+package com.example.todo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.todo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+
+    private SignupRequest register(SignupRequest req) {
+        if (userRepo.existsByUsername(req.getUsername())) {
+            throw new RuntimeException("Username already taken");
+        } 
+        return req;
+}
+
+    private static class SignupRequest {
+
+        public SignupRequest() {
+        }
+
+        private String getUsername() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+}
+
